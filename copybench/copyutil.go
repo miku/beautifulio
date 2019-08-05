01643f8746f505637016ec41036a4bfc2fe2e77b
Martin Czygan <martin.czygan@gmail.com>
2019-08-05 13:44:43 +0200

    various sizes

--- copybench/copyutil.go
+++ copybench/copyutil.go
@@ -32,3 +32,28 @@ func (f *randFile) Read(p []byte) (int, error) {
 	f.I += len(p)
 	return f.r.Read(p)
 }
+
+// emptyFile returns zero file of a given size.
+type emptyFile struct {
+	Size  int
+	I     int
+	Zeros []byte
+}
+
+func EmptyFile(size int) *emptyFile {
+	return &emptyFile{
+		Size:  size,
+		I:     0,
+		Zeros: make([]byte, 16777216),
+	}
+}
+
+// Read fills p with zeros.
+func (f *emptyFile) Read(p []byte) (int, error) {
+	if f.I >= f.Size {
+		return 0, io.EOF
+	}
+	f.I += len(p)
+	copy(p, f.Zeros)
+	return len(p), nil
+}
--- copybench/copyutil_test.go
+++ copybench/copyutil_test.go
@@ -1,21 +1,41 @@
 package copybench
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"testing"
 )
 
-const M = 1024 * 1024
+const (
+	K = 1024
+	M = 1024 * K
+	G = 1024 * M
+)
 
 func TestSimple(t *testing.T) {
-	size := 100 * M
-	f := RandFile(size)
+	size := 1 * M
+	f := EmptyFile(size)
 	k, err := io.Copy(ioutil.Discard, f)
 	if err != nil {
 		t.Errorf("want %v, got %v", nil, err)
 	}
 	if k != int64(size) {
 		t.Errorf("want %d, got %d", size, k)
+	}
+}
+
+func BenchmarkSimple(b *testing.B) {
+	sizes := []int{1 * M, 10 * M, 100 * M, 1 * G, 10 * G, 50 * G}
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("size-%d", size), func(sb *testing.B) {
+			f := EmptyFile(size)
+			for i := 0; i < sb.N; i++ {
+				_, err := io.Copy(ioutil.Discard, f)
+				if err != nil {
+					b.Errorf("want %v, got %v", nil, err)
+				}
+			}
+		})
 	}
 }

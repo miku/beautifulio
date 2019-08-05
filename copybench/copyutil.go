036fdd46b9dd0e2a92b9aae81cdda83e2503c9f4
Martin Czygan <martin.czygan@gmail.com>
2019-08-05 13:22:15 +0200

    copy example

--- copybench/copyutil.go
+++ copybench/copyutil.go
@@ -1,5 +1,34 @@
 package copybench
 
-func Hello(a int) int {
-	return a + 1
+import (
+	"crypto/rand"
+	"io"
+)
+
+type randFile struct {
+	Size int
+	I    int
+	r    io.Reader
+}
+
+func RandFile(size int) *randFile {
+	return &randFile{
+		Size: size,
+		I:    0,
+		r:    rand.Reader,
+	}
+}
+
+// Read reads random bytes.
+func (f *randFile) Read(p []byte) (int, error) {
+	if f.I >= f.Size {
+		return 0, io.EOF
+	}
+	gap := f.Size - f.I
+	if gap < len(p) {
+		f.I += gap
+		return f.r.Read(p[:gap])
+	}
+	f.I += len(p)
+	return f.r.Read(p)
 }
--- copybench/copyutil_test.go
+++ copybench/copyutil_test.go
@@ -1,7 +1,21 @@
 package copybench
 
-import "testing"
+import (
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+const M = 1024 * 1024
 
 func TestSimple(t *testing.T) {
-	t.Log("Hello")
+	size := 100 * M
+	f := RandFile(size)
+	k, err := io.Copy(ioutil.Discard, f)
+	if err != nil {
+		t.Errorf("want %v, got %v", nil, err)
+	}
+	if k != int64(size) {
+		t.Errorf("want %d, got %d", size, k)
+	}
 }

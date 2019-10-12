4dac6253a8fbce06ca85b8f2084480b54941d4f2
Martin Czygan <martin.czygan@gmail.com>
2019-10-12 14:05:28 +0200

    add pipe example

---
+++ x/simplepipe/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	pr, pw := io.Pipe()
+
+	// A group of one?
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	log.Printf("G1=%d", runtime.NumGoroutine())
+
+	go func() {
+		defer wg.Done()
+		if _, err := io.Copy(os.Stdout, pr); err != nil {
+			log.Fatal(err)
+		}
+		// There seems to be a scheduling point here.
+		log.Println("Exiting")
+	}()
+
+	log.Printf("G2=%d", runtime.NumGoroutine())
+
+	if _, err := io.WriteString(pw, "Hello World\n"); err != nil {
+		log.Fatal(err)
+	}
+
+	// We cannot guaratee, that this is executed after wg.Done(), so we get a
+	// deadlock. This waits already, but the other thread hat not been
+	// scheduled yet.
+
+	log.Printf("G3=%d", runtime.NumGoroutine())
+
+	// We cannot thread this, either. We need to thread out the write.
+	wg.Wait()
+
+	// What does wg.Wait do to the scheduler? You could think it would stop this G and let the other run?
+}
